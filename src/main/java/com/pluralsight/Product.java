e981aa7fa60301106b51c82aaa3fc79b2d083c0d
Muhammad Hamza <muhammad.hamza6415@gmail.com>
2023-10-20 14:05:27 -0400

    created product.java

--- src/main/java/com/pluralsight/Product.java
+++ src/main/java/com/pluralsight/Product.java
@@ -1,4 +1,80 @@
 package com.pluralsight;
 
+import java.lang.reflect.Constructor;
+
 public class Product {
+
+    private String name;
+    private double price;
+    private String department;
+    private String id;
+
+//_________________________//
+//    Constructor
+//________________________//
+
+
+    public Product(String name, double price, String department, String id) {
+        this.name = name;
+        this.price = price;
+        this.department = department;
+        this.id = id;
+    }
+
+
+//_________________________//
+//    Getters & Setters
+//________________________//
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+
+//_________________________//
+//    Additional Methods
+//________________________//
+
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", department='" + department + '\'' +
+                ", id='" + id + '\'' +
+                '}';
+    }
+
+
 }
